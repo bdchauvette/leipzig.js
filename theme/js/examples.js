1ab52f3964db8326ea21316124afd785c618bede
Ben Chauvette <bdchauvette@gmail.com>
2015-07-28 16:07:42 +0200

    Update index and example pages

--- theme/js/examples.js
+++ theme/js/examples.js
@@ -6,6 +6,7 @@
     Leipzig('#nyanja'),
     Leipzig('#spanish', { lastLineFree: false }),
     Leipzig('#nahuatl', { firstLineOrig: true, spacing: false }),
+    Leipzig('#ainu', { firstLineOrig: true, spacing: false }),
     Leipzig('#japanese', { firstLineOrig: true, lastLineFree: false}),
     Leipzig('#klingon', { firstLineOrig: true })
   ];
